5ca3b6c2c7164cbae722d340f709ee1d30ed212e
unknown <nguyenhung090399@gmail.com>
2024-02-18 18:24:45 +0700

    show list product

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,28 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Header/Header';
 import Home from './pages/Home/Home';
 import Shop from './pages/Shop/Shop';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { handleProductsFetchRequest } from './features/Redux/Reducers/productSlice';
+import { Grid } from '@mui/material';
 
 
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(handleProductsFetchRequest());
+  }, []);
   return (
     <Router>
       <Navbar />
-      <div className="container mt-5">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          {/* <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/blog" element={<Blog />} /> */}
-          <Route path="/shop" element={<Shop />} />
-        </Routes>
-      </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/blog" element={<Blog />} /> */}
+            <Route path="/shop" element={<Shop />} />
+          </Routes>
     </Router>
 
     
